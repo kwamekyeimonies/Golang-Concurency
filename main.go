22de2392a7c9bbb569743fc12ad53700b59a5a25
Tenkorang Daniel <kwamekyeimonies@gmail.com>
2023-03-27 14:18:44 +0000

    intro conc

--- main.go
+++ main.go
@@ -5,71 +5,24 @@ import (
 	"time"
 )
 
-// func main() {
-// 	var data int
-// 	go func() {
-// 		data++
-// 	}()
-// 	if data == 0 {
-// 		fmt.Printf("The value is %v\n", data)
-// 	}
-// }
-
-// func main(){
-// 	var data int
-// 	go func ()  {
-// 		data++
-// 	}()
-// 	// time.Sleep(5*time.Second)
-// 	if data==0{
-// 		fmt.Printf("The value is %v \n",data)
-// 	}
-// }
-
-// func main() {
-// 	var data int
-// 	go func() { data++ }()
-// 	if data == 0 {
-// 		fmt.Println("The value is Zero is 0")
-// 	} else {
-// 		fmt.Printf("The value is  %v\n", data)
-// 	}
-// }
-
 func main() {
-	now := time.Now()
-	done := make(chan struct{})
-	go first_task(done)
-	go second_task(done)
-	go third_task(done)
-	go fourth_task(done)
-	time.Sleep(time.Second * 1)
-	<-done
-	<-done
-	<-done
-	<-done
-	fmt.Println("Elapsed: ", time.Since(now))
+
+	start := time.Now()
+	defer func() {
+		fmt.Println(time.Since(start))
+	}()
+
+	Food := []string{"Jollof", "Rice and Stew", "Banku and Tilapia", "Fried Rice"}
+
+	for _, my_food := range Food {
+		go Attacker(my_food)
+	}
+
+	time.Sleep(time.Second * 2)
+
 }
 
-func first_task(done chan struct{}) {
-	time.Sleep(100 * time.Millisecond)
-	fmt.Println("Task 1")
-	done <- struct{}{}
-}
-
-func second_task(done chan struct{}) {
-	time.Sleep(200 * time.Millisecond)
-	fmt.Println("Task 2")
-	done <- struct{}{}
-}
-
-func third_task(done chan struct{}) {
-	fmt.Println("Task 3")
-	done <- struct{}{}
-}
-
-func fourth_task(done chan struct{}) {
-	time.Sleep(100 * time.Millisecond)
-	fmt.Println("Task 4")
-	done <- struct{}{}
+func Attacker(target string) {
+	fmt.Println("Throwing Ninja starts", target)
+	time.Sleep(time.Second)
 }
