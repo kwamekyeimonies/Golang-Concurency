9f21b29acf0b305adab07cbadb2c801523c39222
Tenkorang Daniel <kwamekyeimonies@gmail.com>
2023-03-27 16:53:02 +0000

    switch chan

--- main.go
+++ main.go
@@ -2,18 +2,26 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
+	"time"
 )
 
 func main() {
 
-	channel := make(chan string, 4)
-	channel <- "Buffer Channel"
-	channel <- "Second Channel"
-	channel <- "Third Channel"
-	channel <- "4th Channel"
-	fmt.Println(<-channel)
-	fmt.Println(<-channel)
-	fmt.Println(<-channel)
-	fmt.Println(<-channel)
+	channel := make(chan string)
+	numRounds := 5
+	go Throwing_Start(channel, numRounds)
+	// fmt.Println(<-channel)
+	for message := range channel {
+		fmt.Println(message)
+	}
+}
+
+func Throwing_Start(channel chan string, numRounds int) {
+	rand.Seed(time.Now().UnixNano())
+	for i := 0; i < numRounds; i++ {
+		score := rand.Intn(10)
+		channel <- fmt.Sprintf("You scored: %v", score)
+	}
 
 }
